7f7bb6e6c1809d362adb321f8f144a35f3193477
Jack Yune <jackyune.chua@abssasia.com>
2017-05-22 16:30:49 +0800

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public items = [];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+
+  }
+
+  ionViewDidLoad () {
+    this.items = [
+      // {title: 'hi1', description: 'test1'},
+      // {title: 'hi2', description: 'test2'},
+      // {title: 'hi3', description: 'test3'}
+    ];
+  }
+
+  addItem () {
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+      if(item){
+        this.saveItem(item);
+      }
+    });
+
+    addModal.present();
+  }
+
+  saveItem(item){
+    this.items.push(item);
+  }
+
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+}
